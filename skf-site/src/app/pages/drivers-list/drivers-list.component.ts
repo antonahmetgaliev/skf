914b2c19357cca005c3845f20be2b9fd3c42517c
Anthony <akhmethaliieva@dtek.com>
2026-03-12 17:01:21 +0200

    Removed our championships

--- skf-site/src/app/pages/drivers-list/drivers-list.component.ts
+++ skf-site/src/app/pages/drivers-list/drivers-list.component.ts
@@ -2,7 +2,6 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { BwpApiService, Driver } from '../../services/bwp-api.service';
-import { ChampionshipPodium, SimgridApiService } from '../../services/simgrid-api.service';
 
 @Component({
   selector: 'app-drivers-list',
@@ -12,12 +11,10 @@ import { ChampionshipPodium, SimgridApiService } from '../../services/simgrid-ap
 })
 export class DriversListComponent {
   private readonly api = inject(BwpApiService);
-  private readonly simgridApi = inject(SimgridApiService);
 
   readonly drivers = signal<Driver[]>([]);
   readonly loading = signal(true);
   readonly search = signal('');
-  readonly champions = signal<ChampionshipPodium[]>([]);
 
   readonly filtered = computed(() => {
     const q = this.search().trim().toLowerCase();
@@ -27,20 +24,6 @@ export class DriversListComponent {
     return [...list].sort((a, b) => a.name.localeCompare(b.name));
   });
 
-  readonly resolvedChampions = computed(() => {
-    const idMap = new Map<number, string>();
-    for (const d of this.drivers()) {
-      if (d.simgridDriverId != null) idMap.set(d.simgridDriverId, d.id);
-    }
-    return this.champions().map((c) => ({
-      ...c,
-      podium: c.podium.map((e) => ({
-        ...e,
-        driverUuid: e.simgridDriverId != null ? (idMap.get(e.simgridDriverId) ?? null) : null,
-      })),
-    }));
-  });
-
   constructor() {
     this.api.getDrivers().subscribe({
       next: (drivers) => {
@@ -48,10 +31,6 @@ export class DriversListComponent {
         this.loading.set(false);
       },
       error: () => this.loading.set(false),
-    });
-    this.simgridApi.getChampionshipsPodium().subscribe({
-      next: (data) => this.champions.set(data),
-      error: () => {},
     });
   }
 
