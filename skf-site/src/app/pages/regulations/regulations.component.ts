341a089f21ff3cf2271c9175a08814ebaac5afe9
kyrylotaran <kiriltaran@gmail.com>
2026-04-01 21:20:22 +0300

    Migrate regulations page to shared components
    Uses app-page, app-page-intro, app-card. Removes duplicated card,
    button, hero/brand, and animation styles.

--- skf-site/src/app/pages/regulations/regulations.component.ts
+++ skf-site/src/app/pages/regulations/regulations.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { PageIntroComponent } from '../../components/page-intro/page-intro.component';
+import { PageLayoutComponent } from '../../components/page-layout/page-layout.component';
 
 @Component({
   selector: 'app-regulations',
+  imports: [PageIntroComponent, PageLayoutComponent, CardComponent],
   templateUrl: './regulations.component.html',
   styleUrl: './regulations.component.scss'
 })
