9ba1ef43b9e60c260a78796b4ac3d64c07ba34e3
kyrylotaran <kiriltaran@gmail.com>
2026-04-01 21:20:40 +0300

    Migrate home page to shared components
    Uses app-page, app-card, app-btn. Removes duplicated card, button,
    and page wrapper styles.

--- skf-site/src/app/pages/home-visit/home-visit.component.ts
+++ skf-site/src/app/pages/home-visit/home-visit.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BtnComponent } from '../../components/btn/btn.component';
+import { CardComponent } from '../../components/card/card.component';
+import { PageLayoutComponent } from '../../components/page-layout/page-layout.component';
+import { RecentBroadcastsComponent } from '../../components/recent-broadcasts/recent-broadcasts.component';
 
 @Component({
   selector: 'app-home-visit',
-  imports: [RouterLink],
+  imports: [RouterLink, BtnComponent, CardComponent, PageLayoutComponent, RecentBroadcastsComponent],
   templateUrl: './home-visit.component.html',
   styleUrl: './home-visit.component.scss',
 })
